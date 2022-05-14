9166670109e410170b54a705b1f95d56f111ac2d
Mattadin <m4tt4d1n@gmail.com>
2022-05-14 12:28:26 +1000

    SQL foundations set

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id           INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE roles (
+    id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(20) NOT NULL,
+    salary      INT NOT NULL,
+    dept_id     INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL 
+);
+
+CREATE TABLE employees (
+    id          INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name  VARCHAR(50) NOT NULL,
+    last_name   VARCHAR(50) NOT NULL,
+    role_id     INT,
+    manager_id  INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
